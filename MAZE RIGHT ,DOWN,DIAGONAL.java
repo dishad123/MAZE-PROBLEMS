342726ab8ea48ca69d59d312d341337179654bbd
dishad123 <94884272+dishad123@users.noreply.github.com>
2022-03-18 10:56:10 +0530

    Create MAZE RIGHT ,DOWN,DIAGONAL.java

---
+++ MAZE RIGHT ,DOWN,DIAGONAL.java
@@ -0,0 +1,50 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(count(3,3));
+	
+		path("",3,3);
+		
+	}
+	
+	static int count(int row,int column){
+	    if(row==1 || column==1){
+	        return 1;
+	    }
+	    else
+	    {
+	        int left = count(row-1,column);
+	        int right=count(row,column-1);
+	        int diagonal=0;
+	        if(row>1&&column>1)
+	        diagonal=count(row-1,column-1);
+	        
+	        return left+right+diagonal;
+	    }
+	}
+	    
+	  static void path(String p,int row,int column)
+	    {
+	        if(row==1 && column ==1)
+	        {
+	            System.out.println(p);
+	           return;
+	            
+	            
+	        }
+	        else{
+	            if(row>1)
+	            {
+	                path(p+'D',row-1,column);
+	            }
+	            if(column>1)
+	            {
+	                path(p+'R',row,column-1);
+	            }
+	            if(row>1&&column>1)
+	            {
+	                path(p+'V',row-1,column-1);
+	            }
+	        }
+	    }
+	}
